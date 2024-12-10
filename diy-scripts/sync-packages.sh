66535521585db4d93037892f04398317058feef5
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-12-11 02:12:28 +0800

    Update sync-packages.sh

--- diy-scripts/sync-packages.sh
+++ diy-scripts/sync-packages.sh
@@ -10,5 +10,5 @@ syncpath="/mnt/xiaomeng9597/workdir"
 # 拉取luci-app-usb-printer软件包
 mkdir -p $syncpath/themes/luci-theme-design
 git clone --branch openwrt-23.05 https://github.com/coolsnowwolf/luci
-# ls luci/themes/luci-theme-design
+ls luci/themes
 cp -a luci/themes/luci-theme-design/* $syncpath/themes/luci-theme-design
